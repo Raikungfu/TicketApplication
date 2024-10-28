8fdbcd618919254166ce04f7c1a2de43d8e8e94f
ya3k <truongdinhvan1233@gmail.com>
2024-10-28 17:10:33 +0700

    [feat] update entity

--- TicketApplication/Data/ApplicationDbContext.cs
+++ TicketApplication/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TicketApplication.Models;
 
 namespace TicketApplication.Data
 {
@@ -10,5 +11,26 @@ namespace TicketApplication.Data
         {
 
         }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Event> WorkShops { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Cart>().HasKey(c => new { c.UserId, c.TicketId });
+            builder.Entity<OrderDetail>().HasKey(od => new { od.OrderId, od.TicketId });
+   
+
+        }
+
+
+
     }
 }
