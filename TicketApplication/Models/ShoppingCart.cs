44b04d1e59a873def72640d81bcd51f3fce8f301
Raikungfu <raikungfu@gmail.com>
2024-10-28 19:22:42 +0700

    nn

--- TicketApplication/Models/ShoppingCart.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TicketApplication.Models
-{
-    public class ShoppingCart
-    {
-        public List<CartItem> Items { get; set; } = new List<CartItem>();
-        public decimal Total => Items.Sum(item => item.Price * item.Quantity);
-    }
-
-}
