012e20633362337ca23c0fde6127bfd3e0cb34fe
ya3k <truongdinhvan1233@gmail.com>
2024-10-28 23:39:21 +0700

    [feat] admin crud

---
+++ TicketApplication/Controllers/ZonesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TicketApplication.Data;
+using TicketApplication.Models;
+
+namespace TicketApplication.Controllers
+{
+    public class ZonesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ZonesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Zones
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Zones.ToListAsync());
+        }
+
+        // GET: Zones/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zone = await _context.Zones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (zone == null)
+            {
+                return NotFound();
+            }
+
+            return View(zone);
+        }
+
+        // GET: Zones/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Zones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Price,AvailableTickets")] Zone zone)
+        {
+            if (ModelState.IsValid)
+            {
+                zone.CreatedAt = DateTime.Now;
+                _context.Add(zone);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(zone);
+        }
+
+        // GET: Zones/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zone = await _context.Zones.FindAsync(id);
+            if (zone == null)
+            {
+                return NotFound();
+            }
+            return View(zone);
+        }
+
+        // POST: Zones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Name,Price,AvailableTickets,Id")] Zone zone)
+        {
+            if (id != zone.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var zoneToEdit = _context.Zones.Find(id);
+                    zoneToEdit.Name = zone.Name;
+                    zoneToEdit.Price = zone.Price;
+                    zoneToEdit.AvailableTickets = zone.AvailableTickets;
+
+                    _context.Entry(zoneToEdit).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ZoneExists(zone.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(zone);
+        }
+
+        // GET: Zones/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zone = await _context.Zones
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (zone == null)
+            {
+                return NotFound();
+            }
+
+            return View(zone);
+        }
+
+        // POST: Zones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var zone = await _context.Zones.FindAsync(id);
+            if (zone != null)
+            {
+                _context.Zones.Remove(zone);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ZoneExists(string id)
+        {
+            return _context.Zones.Any(e => e.Id == id);
+        }
+    }
+}
--- TicketApplication/Data/Migrations/20241028154609_int65.Designer.cs
+++ TicketApplication/Data/Migrations/20241028154609_int65.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketApplication.Data;
 
@@ -11,9 +12,11 @@ using TicketApplication.Data;
 namespace TicketApplication.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241028154609_int65")]
+    partial class int65
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
