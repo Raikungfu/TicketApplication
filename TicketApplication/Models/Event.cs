f0717764b108b64975cda318ba8ed15b7be5026e
Raikungfu <raikungfu@gmail.com>
2024-10-28 17:54:35 +0700

    home cart

---
+++ TicketApplication/Models/Event.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketApplication.Models
+{
+    public class Event
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string PriceRange { get; set; }
+        public string Link { get; set; }
+    }
+}
