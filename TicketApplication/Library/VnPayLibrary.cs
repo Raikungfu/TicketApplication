6dd2f3755da7098cc5c48eb7d3752d83c0fb4ace
Raikungfu <raikungfu@gmail.com>
2024-10-29 14:16:12 +0700

    vnpay

--- TicketApplication/Library/VnPayLibrary.cs
+++ TicketApplication/Library/VnPayLibrary.cs
@@ -7,7 +7,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Web;
 
-namespace ClothesStoreMobileApplication.Library
+namespace TicketApplication.Library
 {
     public class VnPayLibrary
     {
